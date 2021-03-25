35f318b9ee50a0d4efea72a0393c9cbd7c1545d8
Clayman <Clayman@DESKTOP-DM8E77U>
2021-03-25 19:45:27 +0200

    DynamicMemory2

--- Pointers/DynamicMemory/main.cpp
+++ Pointers/DynamicMemory/main.cpp
@@ -4,19 +4,26 @@ using namespace std;
 #define tab "\t"
 
 void FillRand(int arr[], const int n);
+void FillRand(int** arr, const int m, const int n);
 void Print(int arr[], const int n);
+void Print(int** arr, const int m, const int n);
 
 void push_back(int*& arr, int& n, int value);
 void push_front(int*& arr, int& n, int value);
+void insert(int*& arr, int& n, int value, int index);
 
 void pop_back(int*& arr, int& n);	//Удаляет элемент с конца массива
 void pop_front(int*& arr, int& n);
+void erase(int*& arr, int& n, int index);
 
 //#define PUSH_BACK
+//#define DYNAMIC_MEMORY_1
+#define DYNAMIC_MEMORY_2
 
 void main()
 {
 	setlocale(LC_ALL, "");
+#ifdef DYNAMIC_MEMORY_1
 	int n;
 	cout << "Введите размер массива: "; cin >> n;
 	int* arr = new int[n] {};
@@ -47,9 +54,51 @@ void main()
 	push_front(arr, n, value);
 	Print(arr, n);
 
+	int index;
+	cout << "Введите добавляемое значение: "; cin >> value;
+	cout << "Введите индекс добавляемого элемента: "; cin >> index;
+	insert(arr, n, value, index);
+	Print(arr, n);
+
 	cout << "Удаление последнего элемента:\n";
 	pop_back(arr, n);
 	Print(arr, n);
+	cout << "Введите индекс удавляемого элемента: "; cin >> index;
+	erase(arr, n, index);
+	Print(arr, n);
+	delete[] arr;
+#endif // DYNAMIC_MEMORY_1
+	/////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////		ОБЪЯВЛЕНИЕ ДВУМЕРНОГО ДИНАМИЧЕСКОГО МАССИВА		/////////////////////
+	/////////////////////////////////////////////////////////////////////////////////////////////
+	int m;	//Количество строк
+	int n;	//Количество элементов cтроки
+	cout << "Введите количество строк: "; cin >> m;
+	cout << "Введите количество элементов строки: "; cin >> n;
+	//1)Создаем массив указателей, и сохраняем его адрес в указатель на указатель:
+	int** arr = new int*[m];
+	//2)Выделяем память под строки двумерного массива:
+	for (int i = 0; i < m; i++)
+	{
+		arr[i] = new int[n] {};
+	}
+
+	/////////////////////////////////////////////////////////////////////////////////////////////
+	/////////////////		ИСПОЛЬЗОВАНИЕ ДВУМЕРНОГО ДИНАМИЧЕСКОГО МАССИВА		/////////////////
+	/////////////////////////////////////////////////////////////////////////////////////////////
+	
+	FillRand(arr, m, n);
+	Print(arr, m, n);
+
+	/////////////////////////////////////////////////////////////////////////////////////////////
+	////////////////////		УДАЛЕНИЕ ДВУМЕРНОГО ДИНАМИЧЕСКОГО МАССИВА		/////////////////
+	/////////////////////////////////////////////////////////////////////////////////////////////
+	//1) Удаление строк:
+	for (int i = 0; i < m; i++)
+	{
+		delete[] arr[i];
+	}
+	//2) Удаление массива указателей:
 	delete[] arr;
 }
 void FillRand(int arr[], const int n)
@@ -59,6 +108,16 @@ void FillRand(int arr[], const int n)
 		arr[i] = rand() % 100;
 	}
 }
+void FillRand(int** arr, const int m, const int n)
+{
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			arr[i][j] = rand() % 100;
+		}
+	}
+}
 void Print(int arr[], const int n)
 {
 	for (int i = 0; i < n; i++)
@@ -66,6 +125,17 @@ void Print(int arr[], const int n)
 		cout << arr[i] <<tab;
 	}
 	cout << endl;
+}
+void Print(int** arr, const int m, const int n)
+{
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << arr[i][j] << tab;
+		}
+		cout << endl;
+	}
 }
 
 void push_back(int*& arr, int& n, int value)
@@ -97,6 +167,26 @@ void push_front(int*& arr, int& n, int value)
 	arr[0] = value;
 	n++;
 }
+void insert(int*& arr, int& n, int value, int index)
+{
+	int* buffer = new int[n + 1]{};
+	/*for (int i = 0; i < index; i++)
+		buffer[i] = arr[i];
+	for (int i = index; i < n; i++)
+		buffer[i + 1] = arr[i];*/
+	for (int i = 0; i < n; i++)
+	{
+		true;
+		if (i < index)buffer[i] = arr[i];else buffer[i + 1] = arr[i];
+		//(i < index) ? buffer[i] = arr[i] : buffer[i + 1] = arr[i];
+		//(i < index ? buffer[i] : buffer[i + 1]) = arr[i];
+		buffer[i < index ? i : i + 1] = arr[i];
+	}
+	delete[] arr;
+	arr = buffer;
+	arr[index] = value;
+	n++;
+}
 
 void pop_back(int*& arr, int& n)
 {
@@ -113,4 +203,24 @@ void pop_back(int*& arr, int& n)
 void pop_front(int*& arr, int& n)
 {
 	
+}
+
+void erase(int*& arr, int& n, int index)
+{
+	int* buffer = new int[--n]{};
+	/*for (int i = 0; i < index; i++)
+	{
+		buffer[i] = arr[i];
+	}
+	for (int i = index; i < n; i++)
+	{
+		buffer[i] = arr[i + 1];
+	}*/
+	for (int i = 0; i < n; i++)
+	{
+		//buffer[i] = i < index ? arr[i] : arr[i + 1];
+		buffer[i] = arr[i < index ? i : i + 1];
+	}
+	delete[] arr;
+	arr = buffer;
 }
--- Pointers/HomeWork/main.cpp
+++ Pointers/HomeWork/main.cpp
@@ -1,102 +1,190 @@
-/*
-1. Код добавления значения в конец массива вынести в функцию ??? push_back(???);  +
-2. В проект DynamicMemory добавить функции:
-	??? push_front(???);	//Добавляет значение в начало массива  +
-	??? insert(???);		//Вставляет значение в массив по указанному индексу
-	??? pop_back(???);		//Удаляет последний элемент из массива  +
-	??? pop_front(???);		//Удаляет нулевой элемент из массива
-	??? erase(???);			//Удаляет элемент из массива по заданному индексу
-	Для того чтобы удалить элемент из массива, в буферный массив нужно скопировать все кроме удаляемого элемента.*/
 
-#include<iostream>
+#include <iostream>
+#include <time.h>
+#include <random>
 using namespace std;
-#define tab "\t"
+/*eсть массив 'arr' из 10-ти элементов, заполненный случайными числами. Нужно создать еще два массива 'even' и 'odd' минимально возможного размера, в массив 'even' нужно скопировать все четные значения из исходного массива, а в массив
+'odd' - нечетные. Массивы 'even' и 'odd' вывести на экран.*/
 
-void FillRand(int arr[], const int n);
-void Print(int arr[], const int n);
+void FillRand(int arr[], int n);
+void print(int arr[], int n);
 void push_back(int*& arr, int& n, int value);
 void push_front(int*& arr, int& n, int value);
+
 void pop_back(int*& arr, int& n);
+void pop_front(int*& arr, int& n);
 
+void erase(int*& arr, int& n);
 
-void main()
+void insert(int*& arr, int& n);
+
+int main()
 {
 	setlocale(LC_ALL, "ru");
-	int n;
-	cout << "Введите размер массива: "; cin >> n;
+	int n = 0;
+	cout << "введите количество элементов массива" << endl;
+	cin >> n;
 	int* arr = new int[n] {};
 	FillRand(arr, n);
-	Print(arr, n);
-	cout << endl;
-
-	int value; //добавляемое значение
-	cout << "Введите добавляемое значение: "; cin >> value;
-
+	print(arr, n);
+	int value;
+	cout << "введите добавляемое значение" << endl;
+	cin >> value;
 	push_back(arr, n, value);
-	Print(arr, n);
+	print(arr, n);
+
 	cout << endl;
 
+	cout << "введите добавляемое значение" << endl;
+	cin >> value;
 	push_front(arr, n, value);
-	Print(arr, n);
-	cout << endl;
+	print(arr, n);
 
+
+	cout << "удаление последнего элемента" << endl;
 	pop_back(arr, n);
-	cout << "Последний элемент массива удален: " << endl;
-	Print(arr, n);
+	print(arr, n);
+
+	cout << "удаление нулевого элемента" << endl;
+	pop_front(arr, n);
+	print(arr, n);
+
+	cout << "функция erase удаление элемента по выбранному индексу " << endl;
+	erase(arr, n);
+	print(arr, n);
+
+	cout << "функция insert удаление элемента по выбранному индексу " << endl;
+	insert(arr, n);
+	print(arr, n);
 
 	delete[] arr;
+
 }
 
-void FillRand(int arr[], const int n) //заполнение рандомными значениями
+void FillRand(int* arr, int n)
+{
+	srand(time(NULL));
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % 20;
+	}
+
+}
+void print(int* arr, int n)
 {
 	for (int i = 0; i < n; i++)
 	{
-		arr[i] = rand() % 100;
+		cout << arr[i] << " ";
 	}
+	cout << endl;
 }
 
-void Print(int arr[], const int n) //вывод
+void push_back(int*& arr, int& n, int value)
 {
+	// 1) создаем буферны массив нужного размера
+	int* buffer = new int[n + 1]{};
+	// 2) копируем все значения из одного массва в другой 
 	for (int i = 0; i < n; i++)
 	{
-		cout << arr[i] << tab;
-	}
-}
+		buffer[i] = arr[i];
 
-void push_back(int *&arr, int &n, int value) //Добавляет значение в конец массива
+	}
+	// 3) После того как данные скопированы, удаляем исходный массв
+	delete[] arr;
+	//4) подменяем адрес указателя на исходный массив
+	arr = buffer;
+	//5) И только после всего этого можно записать новое значение а конец массива
+	arr[n] = value;
+	n++;
+}
+void push_front(int*& arr, int& n, int value)
 {
 	int* buffer = new int[n + 1]{};
+	for (int i = 0; i < n; i++)
+	{
+		// копируем элементы со смещением на один элемент вправо
+		buffer[i + 1] = arr[i];
+
+	}
+	delete[] arr;
+	arr = buffer;
+	arr[0] = value;
+	n++;
+}
+
+void pop_back(int*& arr, int& n)
+{
+	int* buffer = new int[--n]{};
+	//для того, чтобы удалить элемент из массива в буферный массив нужно скопировать все элементы кроме удаляемого
 	for (int i = 0; i < n; i++)
 	{
 		buffer[i] = arr[i];
 	}
 	delete[] arr;
 	arr = buffer;
-	buffer[n] = value;
-	n++;
+
 
 }
 
-void push_front(int*& arr, int& n, int value) //Добавляет значение в начало массива
+void pop_front(int*& arr, int& n)
 {
-	int* buffer = new int[n + 1]{};
-	for (int i = 0; i < n; i++)
-	{
-		buffer[i + 1] = arr[i];
-	}
-	buffer[0] = value;
-	delete[] arr;
-	arr = buffer;
-}
-
-void pop_back(int*& arr, int& n) //Удаляет последний элемент из массива
-{
-	//n--;
 	int* buffer = new int[--n]{};
 	for (int i = 0; i < n; i++)
 	{
-		buffer[i] = arr[i];
+		buffer[i] = arr[i + 1];
 	}
 	delete[] arr;
 	arr = buffer;
 }
+
+/*Удаляет элемент из массива по заданному индексу*/
+
+void erase(int*& arr, int& n)
+{
+	int* buffer = new int[n - 1]{};
+	int a;
+	cout << "введите номер элемента для удаления" << endl;
+	cin >> a;
+	for (int i = 0, j = 0; i < n; j++, i++)
+	{
+		if (i != (a - 1))
+		{
+			buffer[j] = arr[i];
+		}
+		else
+		{
+			j--;
+		}
+
+	}
+	n--;
+	delete[] arr;
+	arr = buffer;
+}
+
+//добавить 
+void insert(int*& arr, int& n)
+{
+	int* buffer = new int[n + 1]{};
+	int q;
+	cout << "введите индекс для добавления числа" << endl;
+	cin >> q;
+	int a;
+	cout << "введите число которое вы хотите вставить" << endl;
+	cin >> a;
+	for (int i = 0; i < q; i++)
+	{
+		buffer[i] = arr[i];
+	}
+	buffer[q] = a;
+
+	for (int j = q; j < n; j++)
+	{
+		buffer[j + 1] = arr[j];
+	}
+
+	delete[] arr;
+	arr = buffer;
+	n++;
+}
+
