88789ba682a5968ad08581e1df7de73dd038038b
Clayman <Clayman@DESKTOP-649SK3N>
2021-04-25 22:10:04 +0300

    NULL Terminated day 2

--- NULLTerminatedLines/NULLTerminatedLines/main.cpp
+++ NULLTerminatedLines/NULLTerminatedLines/main.cpp
@@ -7,8 +7,21 @@ int StringLength(char str[]);
 void to_upper(char str[]);	//Переводит строку в верхний регистр
 void to_lower(char str[]);	//Переводит строку в нижний регистр
 void capitalize(char str[]);	//Первую букву каждого слова в предложении делает заглавной
+void shrink(char str[]);
+void remove_symbol(char str[], char symbol);
+
+bool is_palindrome(char str[]);
+
+bool is_int_number(char str[]);
+int  to_int_number(char str[]);
+
+bool is_bin_number(char str[]);
+char* dec_to_bin(int decimal);
+int  bin_to_dec(char str[]);
 
 //#define STRING_DECLARATION //Объявление строк
+//#define CHECK_1
+#define BINARY
 
 void main()
 {
@@ -29,24 +42,42 @@ void main()
 	//ASCII();
 	cout << (int)'a' - (int)'A' << endl;
 	cout << 'a' - 'A' << endl;
-	const int n = 20;
-	char str[n] = {};
-	cout << "Введите строку: ";
-	//cin >> str1;
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
-	cin.getline(str, n);//CP1251
-	cout << str << endl;//CP866
+	const int n = 256;
+	//char str[n] = "Hello   World     (  это    Привет   -     Мир   !!!          )";
+	char str[n] = "Аргентина манит негра";
+	//cout << "Введите строку: ";
+	////cin >> str1;
+	//SetConsoleCP(1251);
+	//SetConsoleOutputCP(1251);
+	//cin.getline(str, n);//CP1251
+	//cout << str << endl;//CP866
+#ifdef CHECK_1
 	cout << StringLength(str) << endl;
 	//cout << (int)'0' << endl;
-	to_upper(str);
-	capitalize(str);
+	//to_upper(str);
+	//capitalize(str);
+	//shrink(str);
 	cout << str << endl;
+
+	cout << "Строка " << (is_palindrome(str) ? "" : "НЕ ") << "является палиндромом" << endl;
+#endif // CHECK_1
+
+	//cout << "Строка " << str << (is_int_number(str) ? "" : " НЕ") << " является целым числом" << endl;
+	//cout << str << endl;
+	//cout << to_int_number(str) * 2 << endl;
+
+	//cout << is_bin_number(str) << endl;
+
+	int decimal;
+	cout << "Введите десятичное число: "; cin >> decimal;
+	cout << dec_to_bin(decimal) << endl;
+
+	//	cout << str << "(bin) = " << bin_to_dec(str) << "(dec)\n";
 }
 
 void ASCII()
 {
-	for (int i = 0; i < 256; i++)
+	for (int i = 0; i < 128; i++)
 	{
 		cout << i << "\t" << (char)i << endl;
 	}
@@ -110,4 +141,165 @@ void capitalize(char str[])
 				str[i] -= 32;
 		}
 	}
+}
+
+void shrink(char str[])
+{
+	//Общий случай:
+	for (int i = 0; str[i]; i++)
+	{
+		if (
+			str[i] == ' ' &&
+			(
+				str[i + 1] == ' ' ||
+				str[i + 1] == '.' ||
+				str[i + 1] == ',' ||
+				str[i + 1] == '?' ||
+				str[i + 1] == '!' ||
+				str[i + 1] == ':' ||
+				str[i + 1] == ';' ||
+				str[i + 1] == '-' ||
+				str[i + 1] == ')'
+				)
+			|| str[0] == ' '
+			|| str[i - 1] == '(' && str[i] == ' '
+			|| str[i - 1] == '-'&& str[i] == ' '
+			)
+		{
+			for (int j = i; str[j]; j++)
+			{
+				str[j] = str[j + 1];
+			}
+			i--;
+		}
+	}
+}
+
+void remove_symbol(char str[], char symbol)
+{
+	for (int i = 0; str[i]; i++)
+	{
+		while (str[i] == symbol)
+		{
+			for (int j = i; str[j]; j++)
+			{
+				str[j] = str[j + 1];
+			}
+		}
+	}
+}
+
+bool is_palindrome(char str[])
+{
+	int length = StringLength(str);
+	char* buffer = new char[length + 1]{};
+	for (int i = 0; i < length; i++)
+	{
+		buffer[i] = str[i];
+	}
+	to_lower(buffer);
+	remove_symbol(buffer, ' ');
+	length = StringLength(buffer);
+	bool is_palindrome = true;
+	for (int i = 0; i < length / 2; i++)
+	{
+		if (buffer[i] != buffer[length - 1 - i])
+		{
+			is_palindrome = false;
+			break;
+		}
+	}
+	delete[] buffer;
+	return is_palindrome;
+}
+
+bool is_int_number(char str[])
+{
+	for (int i = 0; str[i]; i++)
+	{
+		//if (!(str[i] >= '0' && str[i] <= '9'))
+		if ((str[i] < '0' || str[i] > '9') && str[i] != ' ')return false;
+		if (str[i] == ' ' && str[i + 1] == ' ')return false;
+	}
+	return true;
+}
+
+int  to_int_number(char str[])
+{
+	if (!is_int_number(str))return 0;
+	int decimal = 0;	//Десятичное число
+	for (int i = 0; str[i]; i++)
+	{
+		if (str[i] >= '0' && str[i] <= '9')
+		{
+			decimal *= 10;	//Сдвигаем число на 1 разряд влево
+			decimal += str[i] - '0';
+		}
+	}
+	return decimal;
+}
+
+bool is_bin_number(char str[])
+{
+	for (int i = 0; str[i]; i++)
+	{
+		if (str[i] != '0' && str[i] != '1' && str[i] != ' ')
+			return false;
+		if (str[i] == ' ' && str[i - 1] == ' ' && str[i + 1] == ' ')
+			return false;
+	}
+	return true;
+}
+
+char* dec_to_bin(int decimal)
+{
+	//1) Определим количество двоичных разрядов:
+	int capacity = 0;
+	int buffer = decimal;
+	for (; buffer > 0; capacity++)
+	{
+		buffer /= 2;
+		if (capacity % 4 == 0)capacity++;
+	}
+	//2) Выделяем память под двоичное число:
+	char* bin = new char[capacity + 1]{};
+	//3) Получаем разряды двоичного числа, и сохраняем их в строку:
+	for (int i = 0; decimal; i++)
+	{
+		if (i % 4 == 0)
+		{
+			bin[i] = ' ';
+		}
+		else
+		{
+			bin[i] = decimal % 2 + '0';	//Получаем младший разряд числа:
+			decimal /= 2;				//Убираем младший разряд из числа
+		}
+	}
+	return bin;
+}
+
+int bin_to_dec(char str[])
+{
+	if (!is_bin_number(str))return 0;
+#ifdef DEBUG
+	int decimal = 0;	//Конечное десятичное число
+	int weight = 1;		//Весовой коэффициент разряда 2^n;
+	int capacity = strlen(str);	//Разрядность двоичного числа
+	for (int i = capacity - 1; i >= 0; i--)
+	{
+		decimal +=
+	}
+#endif // DEBUG
+
+	int decimal = 0;
+	for (int i = 0; str[i]; i++)
+	{
+		if (str[i] != ' ')
+		{
+			decimal *= 2;
+			decimal += str[i] - '0';
+		}
+	}
+	return decimal;
 }
